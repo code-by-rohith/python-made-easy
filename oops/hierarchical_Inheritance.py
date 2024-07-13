a4ff8bd86b0746f75271f38e3b9815423dce2d85
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-13 21:23:14 +0530

    add

---
+++ oops/hierarchical_Inheritance.py
@@ -0,0 +1,20 @@
+class vehicle:
+    wheels=True
+    @property
+    def move_forword(self):
+        print("Moving  Forword")
+
+class car(vehicle):
+    no_of_wheels=4
+class bike(vehicle):
+    no_of_wheels=2
+
+car1=car()
+print("Wheels : ",car1.wheels)
+print("No of wheels :",car1.no_of_wheels)
+car1.move_forword
+print("")
+bike1=bike()
+print("wheels : ",bike1.wheels)
+print("No of wheels :",bike1.no_of_wheels)
+bike1.move_forword
