6f5d99191eda6ef2f9c63b334006f245d9c4898b
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-03 20:29:39 +0530

    add

--- DataAnalytics/NumpyND.py
+++ DataAnalytics/NumpyND.py
@@ -122,7 +122,6 @@ arr1 = np.array([1, 2, 3])
 arr2 = np.array([4, 5, 6])
 arr = np.concatenate((arr1, arr2))
 print(arr)
-++
 arr1 = np.array([[1, 2], [3, 4]])
 arr2 = np.array([[5, 6], [7, 8]])
 arr = np.concatenate((arr1, arr2), axis=0)
